8085633c1171b9fd8bd94c0fc1b20f3643737911
Taalaybelekov <tito_b@mail.ru>
2022-09-23 20:23:40 +0600

    added numbers

---
+++ types/numbers.py
@@ -0,0 +1,69 @@
+# Типы данных
+# int -целочиленные
+# float - 2,553  :
+# number = 5 # (переменная)
+# print(number)
+# number = 12
+# print(number)
+# a = 5
+# b =15
+# result =a+b
+# print(result)
+# abc - нижний регистр
+# ABC - верхний регистр
+# a = 100
+# b = 1000
+# c = 500
+# d = 56.20
+# print(a+b+c+d)
+#
+# nam1 = 996
+# nam2 = 67.55
+# print(nam1-nam2)
+# *
+# num1 = 55
+# num2 = 199988
+# print('результат', num1*num2)
+# num1 = input('Vedite chislo')
+# print(type(num1))
+# num = input('vedite chislo') #-Любой цифра текст когда запрашиваешь он сохранает в строку
+# print(type(num))
+# num = int(input('vedite chislo'))
+# print(type(num))
+# num1 = int(input('Vedite chislo'))
+# num2 = int(input('Vedite chislo'))
+# result = num1*num2
+# print(result)
+# / обычное деление
+# // целочисленное деление (деление без остатка)
+# % - Модульное деление (возваращяет остаток от деления)
+# num1 = 25
+# num2 = 12
+# print(num1 / num2)
+# print( num1 // num2)
+# print(num1 % num2)
+# ** -возвидение степень
+# num1 = 5
+# print(num1 ** 100)
+# pow(a,b)- функция возвидение степень а возводит на b
+# print(pow(5, 3))
+# print(pow(5, 3, 13)) #5**3/13
+# divmod(a, b) оно выводит 2 чила
+# первое это результат целочисленного деления (//),
+# а второе это модульное деление (%)
+# res = divmod( 32, 2)
+# print(res)
+# round()- округление
+# res = 24 / 13
+# print(round(res))
+# print(round(res, 3))
+# abs()- абсолютное значение  abs(-5) = 5 всегда возврашает положительное число
+# print(abs(-100))
+# print(abs(50))
+# import math
+# print(math.pi)
+# print(math.sqrt(16)) # корень
+# print(math.sqrt(101))
+# from math import pi ,sqrt #- можно импортировать отдельные модули
+# print(pi)
+# print(sqrt(100))
